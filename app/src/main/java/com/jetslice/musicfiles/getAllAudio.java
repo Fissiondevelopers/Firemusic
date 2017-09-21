872b367f846cd3cb927020ca1ba279dc1f91bf9b
Aryan Soni <aryansoni1108@gmail.com>
2017-09-22 00:29:24 +0530

    Reading of music files

---
+++ app/src/main/java/com/jetslice/musicfiles/getAllAudio.java
@@ -0,0 +1,59 @@
+package com.jetslice.musicfiles;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class getAllAudio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_all_audio);
+        getAllAudioFromDevice(getAllAudio.this);
+    }
+    public List<AudioModel> getAllAudioFromDevice(final Context context) {
+
+        final List<AudioModel> tempAudioList = new ArrayList<>();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {MediaStore.Audio.AudioColumns.DATA, MediaStore.Audio.AudioColumns.ALBUM, MediaStore.Audio.ArtistColumns.ARTIST,};
+        Cursor c = context.getContentResolver().query(uri,   projection,
+                null,
+                null,
+                null);
+
+        if (c != null) {
+            while (c.moveToNext()) {
+
+                AudioModel audioModel = new AudioModel();
+                String path = c.getString(0);
+                String album = c.getString(1);
+                String artist = c.getString(2);
+
+
+                String name = path.substring(path.lastIndexOf("/") + 1);
+
+                audioModel.setaName(name);
+                audioModel.setaAlbum(album);
+                audioModel.setaArtist(artist);
+                audioModel.setaPath(path);
+
+                Log.e("Name :" + name, " Album :" + album);
+                Log.e("Path :" + path, " Artist :" + artist);
+
+                tempAudioList.add(audioModel);
+            }
+            c.close();
+        }
+
+        return tempAudioList;
+    }
+}
